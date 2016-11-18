33a961bbb944b5d25a33b518ae39409b0bb29580
Travis <Travis@Traviss-MacBook-Pro.local>
2016-11-17 19:27:51 -0500

    express

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-var http = require('http');
-var quotes = require('./export');
-http.createServer(onRequest).listen(1111);
-function onRequest(request, response) {
-  var random = quotes[Math.floor(Math.random() * quotes.length)];
-response.writeHead(200, { "Content-type": "text/plain" });
-response.write(random);
-response.end();
-}
-console.log(quotes);
-
 var express = require('express');
 var app = express();
-// respond with "Hello World!" on the homepage
-app.get('/api/hello', function (req, res) {
-res.send('Hello World!');
+var quotes = require('./quotes');
+
+app.get('/quotes', function(req, res){
+  var chosenString = quotes[Math.floor(Math.random()*lyrics.quotes)];
+  res.send(chosenString);
 });
-var server = app.listen(3000, function () {
-var host = server.address().address;
-var port = server.address().port;
-console.log('Example app listening at http://%s:%s', host, port);
+
+app.get('/api/quotes', function(req, res){
+
+  res.send(quotes);
+});
+
+var server = app.listen(1234, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Server is running");
 });
